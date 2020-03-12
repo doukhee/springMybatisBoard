355ff273f6c1b9f7a973d82312ff1f444f6aad49
duckhee <fain9301@yahoo.com>
2020-03-12 18:30:56 +0900

    test

---
+++ Mybatis-Board/src/main/java/kr/co/portfolio/admin/user/service/AdminUserServiceImpl.java
@@ -0,0 +1,8 @@
+package kr.co.portfolio.admin.user.service;
+
+import org.springframework.stereotype.Service;
+
+@Service(value="AdminUserService")
+public class AdminUserServiceImpl implements AdminUserService {
+
+}
